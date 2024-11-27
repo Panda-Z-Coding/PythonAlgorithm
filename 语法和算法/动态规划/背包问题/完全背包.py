902416f34d4ef84ae9c089728bde08a513b5ebfc
panda <2558891266@qq.com>
2024-11-28 00:11:52 +0800

    11/18

---
+++ 语法和算法/动态规划/背包问题/完全背包.py
@@ -0,0 +1,35 @@
+'''
+有无数个物品
+'''
+
+#! dp[i][j] 前i种物品，体积为j的最大值
+# 不拿: dp[i][j] = dp[i-1][j]
+# 拿: dp[i][j] = max(dp[i-1][j-wi] + vi, dp[i-1][j-2wi] + 2vi,...,dp[i-1][j-kwi] + kvi)
+
+# 三中循环
+
+# 优化
+# dp[i][j] = max(dp[i-1][j], dp[i][j-w] + v)
+
+N, V = map(int, input().split())
+# dp = [[0] * (V + 1) for i in range(N + 1)]
+
+'''
+for i in range(1, N + 1):
+    w, v = map(int, input().split())
+    for j in range(0, V + 1):
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j - w] + v)
+print(dp[N][V])
+'''
+# 一维数组更新
+dp = [0] * (V + 1)
+for _ in range(N):
+    w, v = map(int, input().split())
+    #? 后面的是要基于前面的推出来的, 所以从前面往后更新
+    # 要是01背包的话, for i in range(V + 1, w, -1)
+    for j in range(w, V + 1):
+        dp[j] = max(dp[j], dp[j - w] + v)
+print(dp[V])
